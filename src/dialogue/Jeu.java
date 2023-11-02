a7ad9fbd84ce093c359b3cce73015f0df175fd61
Hippolyte Handwerk <hippolyte.handwerk@gmail.com>
2023-11-02 19:50:52 +0100

    debut Avion+jeu

---
+++ src/dialogue/Jeu.java
@@ -0,0 +1,24 @@
+package dialogue;
+
+import java.util.Random;
+
+import jeu.Avion;
+
+public class Jeu {
+	
+	private Avion avions[];
+	private Avion avionEnCours;
+	
+	public Jeu(Avion avions[]) {
+		this.avions=avions;
+	}
+	
+	public void newAvion() {
+		avionEnCours=avions[0];
+	}
+	
+	
+	public boolean isReponseJuste(String reponseType,String reponseVariante) {
+		return avionEnCours.getType().contains(reponseType.toLowerCase())&&avionEnCours.getVariante().contains(reponseVariante.toLowerCase());
+	}
+}
--- src/jeu/Avion.java
+++ src/jeu/Avion.java
@@ -4,8 +4,45 @@ import java.awt.Image;
 import java.util.Collection;
 
 public class Avion {
-	private String reponse;
+	private Collection<String> type;
+	private Collection<String> variante;
 	private Pays pays;
-	private String indices[];
+	private String[] indices;
+	
+	
+	public Avion(Collection<String> type, Collection<String> variante, Pays pays, String[] indices) {
+		super();
+		this.type = type;
+		this.variante = variante;
+		this.pays = pays;
+		this.indices = indices;
+	}
+	
+	public Collection<String> getType() {
+		return type;
+	}
+	public void setType(Collection<String> type) {
+		this.type = type;
+	}
+	public Collection<String> getVariante() {
+		return variante;
+	}
+	public void setVariante(Collection<String> variante) {
+		this.variante = variante;
+	}
+	public Pays getPays() {
+		return pays;
+	}
+	public void setPays(Pays pays) {
+		this.pays = pays;
+	}
+	public String[] getIndices() {
+		return indices;
+	}
+	public void setIndices(String[] indices) {
+		this.indices = indices;
+	}
+	
+	
 	
 }
